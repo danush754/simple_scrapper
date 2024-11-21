5d6ce17c1baccb4ab48d26d0a7e598f3e479b005
dhanush <danush2228s@gmail.com>
2024-11-21 08:19:14 +0530

    completed my simple scrapper and understood what is the use of scrapper

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/danush754/simple_scrapper
 
 go 1.23.0
+
+require golang.org/x/net v0.31.0 // indirect
--- main.go
+++ main.go
@@ -2,8 +2,74 @@ package main
 
 import (
 	"fmt"
+	"net/http"
 	"os"
+	"strings"
+
+	"golang.org/x/net/html"
 )
+
+func getHref(tag html.Token) (ok bool, href string) {
+
+	for _, attr := range tag.Attr {
+		if attr.Key == "href" {
+			href = attr.Val
+			ok = true
+		}
+	}
+
+	return
+
+}
+
+func crawl(url string, ch chan string, chFinished chan bool) {
+
+	resp, err := http.Get(url)
+	if err != nil {
+		fmt.Println("Error failed to Crawl")
+		return
+
+	}
+
+	defer func() {
+		chFinished <- true
+	}()
+
+	body := resp.Body
+
+	defer body.Close()
+
+	token := html.NewTokenizer(body)
+
+	for {
+		nextTokens := token.Next()
+
+		switch {
+		case nextTokens == html.ErrorToken:
+			fmt.Println("stuck with the error so returning the program")
+			return
+		case nextTokens == html.StartTagToken:
+			tag := token.Token()
+
+			isAnchortag := tag.Data == "a"
+			if !isAnchortag {
+				continue
+			}
+
+			ok, url := getHref(tag)
+
+			if !ok {
+				continue
+			}
+
+			hasProto := strings.Index(url, "https") == 0
+			if hasProto {
+				ch <- url
+			}
+		}
+	}
+
+}
 
 func main() {
 
@@ -28,8 +94,8 @@ func main() {
 
 	}
 
-	fmt.Println("Found Urls", len(foundUrls), "unique urls:\n")
-	for url, _ := range foundUrls {
+	fmt.Println("Found Urls", len(foundUrls), ", unique urls:")
+	for url := range foundUrls {
 		fmt.Println("-" + url)
 	}
 
